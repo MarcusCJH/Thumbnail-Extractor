71f7c1f1d59103fdd79670a460fde847549d2672
MarcusCJH <MarcusChanJH@gmail.com>
2017-09-29 12:38:17 +0800

    Updated .bat File
    Add in to create directory and save into director.

--- Thumbnail Extractor.bat
+++ Thumbnail Extractor.bat
@@ -1,7 +1,8 @@
-    for /r %%f in (*.mp4) do (
-
-            echo %%f
-	ffmpeg -ss 00:00:01 -i %%f -vframes 1 -q:V 2 %%~nf.jpg
-    )
+mkdir %~dp0\thumbnail
+for /r %%f in (*.mp4) do 
+(
+	echo %%f
+	ffmpeg -ss 00:00:01 -i %%f -vframes 1 -q:V 2 thumbnail\%%~nf.jpg
+)
 pause
 
